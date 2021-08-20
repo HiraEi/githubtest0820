3a25b384e8c75da6fe3042ae33533608c3e17def
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-20 14:30:39 +0900

    cubeを回した

---
+++ Assets/Nakajima.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nakajima : MonoBehaviour
+{
+    [SerializeField] GameObject m_cube = default;
+    
+    void Update()
+    {
+        m_cube.transform.Rotate(1, 0, 0);
+    }
+}
