af288adbc4eacd3b660ca80b63c9b086c5602f67
HiraEi <aeblanket@gmail.com>
2021-08-20 13:14:18 +0900

    cubeとスクリプトを追加した

---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    int a = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("aaa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
